5ff07b8d9f650fd349884002fe1ce80d20e5f307
tanmaydyadav <84184834+tanmaydyadav@users.noreply.github.com>
2024-04-26 15:57:13 +0530

    Update Clients.js

--- backend/Backend/models/Clients.js
+++ backend/Backend/models/Clients.js
@@ -1,1 +1,55 @@
+const mongoose =require('mongoose');
+const {Schema} = mongoose;
+const ClientSchema = new Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    phoneNo:{
+        type:Number,
+        require:true
+        
+    },
+    dob:{
+        type:Date
+    },
+    // address:{
+    //     street:{
+    //         type:String,
+    //         required:true
+    //     },
+    //     city:{
+    //         type:String,
+    //         required:true
+    //     },
+    //     state:{
+    //         type:String,
+    //         required:true
+    //     },
+    //     pincode:{
+    //         type:Number,
+    //         required:true
 
+    //     }
+    // },
+    date:{
+        type:Date,
+        default:Date.now
+    },
+    flag:{
+        type:Boolean,
+        default:false
+    }
+})
+const Client = mongoose.model('client', ClientSchema)
+// User.createIndexes()
+module.exports = Client
